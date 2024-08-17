048618c87a0cd59bed23f242f5cd2283e6b29246
Pavel Dubrova <pashadubrova@gmail.com>
2024-08-17 17:44:23 +0300

    cp_headers: Sync headers with the current kernel version
    Remove the following non-existent headers:
      - msm_audio_calibration.h
      - msm_audio_wmapro.h
      - msm_audio_amrwb.h
      - msm_audio_amrwbplus.h
      - msm_audio_wma.h
      - msm_audio_aac.h
    
    Remove internal header wcd9xxx_registers.h which is not
    used by any external module.
    
    Add new audio_slimslave.h header.
    
    Signed-off-by: Pavel Dubrova <pashadubrova@gmail.com>

--- kernel-headers/sound/audio_slimslave.h
+++ kernel-headers/sound/audio_slimslave.h
@@ -16,22 +16,16 @@
  ***
  ****************************************************************************
  ****************************************************************************/
-#ifndef _UAPI_MSM_AUDIO_WMAPRO_H
-#define _UAPI_MSM_AUDIO_WMAPRO_H
+#ifndef __AUDIO_SLIMSLAVE_H__
+#define __AUDIO_SLIMSLAVE_H__
 #include <linux/types.h>
-#define AUDIO_GET_WMAPRO_CONFIG _IOR(AUDIO_IOCTL_MAGIC, (AUDIO_MAX_COMMON_IOCTL_NUM + 0), struct msm_audio_wmapro_config)
-#define AUDIO_SET_WMAPRO_CONFIG _IOW(AUDIO_IOCTL_MAGIC, (AUDIO_MAX_COMMON_IOCTL_NUM + 1), struct msm_audio_wmapro_config)
-struct msm_audio_wmapro_config {
-  unsigned short armdatareqthr;
-  __u8 validbitspersample;
-  __u8 numchannels;
-  unsigned short formattag;
-  __u32 samplingrate;
-  __u32 avgbytespersecond;
-  unsigned short asfpacketlength;
-  __u32 channelmask;
-  unsigned short encodeopt;
-  unsigned short advancedencodeopt;
-  __u32 advancedencodeopt2;
+#include <linux/ioctl.h>
+#define AUDIO_SLIMSLAVE_IOCTL_NAME "audio_slimslave"
+#define AUDIO_SLIMSLAVE_MAGIC 'S'
+#define AUDIO_SLIMSLAVE_IOCTL_UNVOTE _IO(AUDIO_SLIMSLAVE_MAGIC, 0x00)
+#define AUDIO_SLIMSLAVE_IOCTL_VOTE _IO(AUDIO_SLIMSLAVE_MAGIC, 0x01)
+enum {
+  AUDIO_SLIMSLAVE_UNVOTE,
+  AUDIO_SLIMSLAVE_VOTE
 };
 #endif
